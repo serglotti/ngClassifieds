44a2454195dd86f3e0aeb9bb755f6c1593f9d3e9
serglotti <sergio.lottimore@gmail.com>
2016-04-16 23:41:05 -0300

    setting up ui-routing demo including ui-sref

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,34 @@
 angular
-	.module("ngClassifieds", ["ngMaterial"])
-	.config(function($mdThemingProvider) {
+	.module("ngClassifieds", ["ngMaterial", "ui.router"])
+	.config(function($mdThemingProvider, $stateProvider) {
 		$mdThemingProvider.theme('default')
 			.primaryPalette('pink')
 			.accentPalette('purple');
+
+		$stateProvider
+			.state('stateone', {
+				url: '/stateone',
+				template: '<h1> {{ stateOne.message }} </h1>',
+				controller: 'stateOneCtrl as stateOne'
+			})
+			.state('statetwo', {
+				url: '/statetwo',
+				template: '<h1>State Two</h1> <md-button ui-sref="two.more">Go to nested state</md-button><ui-view></ui-view>'
+			})
+			.state('two.more', {
+				url: '/more',
+				template: '<p>This is a deeper state</p>'
+			});
+		
 	})
-	.directive("helloWorld", function() {
-		return {
-			template: "<h1> {{ message }} </h1>"
-		}
+	.controller('stateOneCtrl', function() {
+		var vm = this; // used to avoid $scope soup from occurring within our app. also eliminates need for $scope to be injected into the function
+		vm.message = 'Hey from State One';
 	});
+
+
+	// .directive("helloWorld", function() {
+	// 	return {
+	// 		template: "<h1> {{ message }} </h1>"
+	// 	}
+	// });
