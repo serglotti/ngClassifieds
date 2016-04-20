410bc347404476f326581499f56a9d1eb8f114af
serglotti <sergio.lottimore@gmail.com>
2016-04-19 21:03:17 -0300

    route refactoring

--- scripts/app.js
+++ scripts/app.js
@@ -6,29 +6,11 @@ angular
 			.accentPalette('purple');
 
 		$stateProvider
-			.state('stateone', {
-				url: '/stateone',
-				template: '<h1> {{ stateOne.message }} </h1>',
-				controller: 'stateOneCtrl as stateOne'
-			})
-			.state('statetwo', {
-				url: '/statetwo',
-				template: '<h1>State Two</h1> <md-button ui-sref="two.more">Go to nested state</md-button><ui-view></ui-view>'
-			})
-			.state('two.more', {
-				url: '/more',
-				template: '<p>This is a deeper state</p>'
-			});
+			.state('classifieds', {
+				url: '/classifieds',
+				templateURL: 'components/classifieds/classifieds.tpl.html',
+				controller: 'classifiedsCtrl as vm' // vm simply stands for view model and is the alias for this controller
+		});
 		
-	})
-	.controller('stateOneCtrl', function() {
-		var vm = this; // used to avoid $scope soup from occurring within our app. also eliminates need for $scope to be injected into the function
-		vm.message = 'Hey from State One';
 	});
-
-
-	// .directive("helloWorld", function() {
-	// 	return {
-	// 		template: "<h1> {{ message }} </h1>"
-	// 	}
-	// });
+ 
